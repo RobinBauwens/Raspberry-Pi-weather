5205c8aedbae1eb436e5c4f860eb30983f778e19
RobinBauwens <robin.bauwens.w0458@student.hogent.be>
2017-08-01 18:22:07 +0200

    Update install_mariadb-server.sh

--- install_mariadb-server.sh
+++ install_mariadb-server.sh
@@ -2,3 +2,10 @@
 
 sudo apt-get update
 sudo apt-get install mariadb-server
+sudo apt-get install python
+sudo apt-get install python-mysqldb
+
+# sudo pip install mysql-connector
+sudo pip install mysql-connector-python-rf
+
+mysql_secure_installation
