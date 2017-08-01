0223054eefa6d28559f960d5ac0a04e300f5e6b4
RobinBauwens <robin.bauwens.w0458@student.hogent.be>
2017-08-01 20:57:43 +0200

    Toevoegingen script

--- install_programs.sh
+++ install_programs.sh
@@ -8,4 +8,8 @@ sudo apt-get install python-mysqldb
 # sudo pip install mysql-connector
 sudo pip install mysql-connector-python-rf
 
+sudo apt-get install apache2 -y
+sudo apt-get install php5 libapache2-mod-php5 -y
+sudo apt-get install php5-mysql
+
 mysql_secure_installation
